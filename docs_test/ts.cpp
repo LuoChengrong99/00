19f8b10d24910d668fcef9fca066b5d1a68afb09
Chengrong Luo <luochengrong99@gmail.com>
2026-01-20 20:32:59 +0800

    add codes

--- docs_test/ts.cpp
+++ docs_test/ts.cpp
@@ -2,6 +2,16 @@
 
 int main() 
 {
+    int i = 1; 
+    int j = 3; 
+    int k_i = i / j; 
+    double k_d0 = i / j; 
+    double k_d1 = 1.0 * i / j; 
+    double k_d2 = static_cast<double>(i) / j; 
+
+    std::cout << k_i << "," << k_d0 << "," << k_d1 << "," << k_d2 << std::endl; 
+
+
     std::cout << "hello python!" << std::endl; 
     std::cout << "111" << std::endl; 
 
